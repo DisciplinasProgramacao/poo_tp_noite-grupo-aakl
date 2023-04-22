c784043e2b01a4c251630b1dd02836fdaac03654
apoliveirapuc <104520144+apoliveirapuc@users.noreply.github.com>
2023-04-22 17:58:38 -0300

    Criação das classes

---
+++ codigo/PlataformaStreaming/src/Cliente.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Cliente {
+    
+    private int login;
+    private String senha;
+    private List<Serie> assistidas = new ArrayList<>();
+    private List<Serie> interesses = new ArrayList<>();
+
+    public Cliente(){
+
+    }
+
+    public Serie buscarSerieAssistida(String nome){
+    
+    }
+
+    public Serie buscarSerieInteresse(String nome){
+
+    }
+
+    public void adicionarAssistida(Serie serie){
+
+    }
+
+    public void adicionarInteresse(Serie serie){
+        
+    }
+
+    public boolean removerInteresse(Serie serie){
+
+    }
+}
---
+++ codigo/PlataformaStreaming/src/Serie.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Serie {
+    
+    private String nome, genero, idioma;
+    private int contReproducoes;
+    private static final List<String> GENEROS = new ArrayList<>();
+    private static final List<String> IDIOMAS = new ArrayList<>();
+
+    public Serie (){
+
+    }
+
+    public void incrementarReproducoes(){
+        
+    }
+}
