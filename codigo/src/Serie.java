085e41db04b3367f98bf2cdf7ae6ee5b895cdc9a
apoliveirapuc <104520144+apoliveirapuc@users.noreply.github.com>
2023-05-20 20:37:37 -0300

    alteração das classes

---
+++ codigo/src/Serie.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Serie {
+    
+    public String nome, genero, idioma;
+    private int contReproducoes;
+    private static final List<String> GENEROS = new ArrayList<>();
+    private static final List<String> IDIOMAS = new ArrayList<>();
+
+    public Serie (){
+
+    }
+
+    public void incrementarReproducoes(){
+        
+    }
+}
